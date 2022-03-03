64f9b5c20c94a2261296f38a747fbfc0ff4509db
darkskull1297 <77977994+darkskull1297@users.noreply.github.com>
2022-03-03 22:45:40 +0100

    Sync

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -8,7 +8,7 @@ local_path=$5
 rm -rf $local_path
 
 # Create a zip of the current directory.
-zip -r $local_path . -x "*.git/"
+zip -r $local_path . -x "*.git/" -x ".git/**"
 
 # Install required dependencies for Python script.
 pip3 install boto3
