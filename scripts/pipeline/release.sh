f1ff3805da85ee5027d51bfb26c736320757c27f
darkskull1297 <77977994+darkskull1297@users.noreply.github.com>
2022-03-03 22:38:01 +0100

    Sync

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -8,7 +8,7 @@ local_path=$5
 rm -rf $local_path
 
 # Create a zip of the current directory.
-zip -r $local_path . -x "*.git/**" -x "*.git/" -x "*.github/**" -x "scripts/**" -x "node_modules/**" -x ".DS_Store"
+zip -r $local_path . -x "*.git/**" -x "*.git/" -x "*.github/" -x "scripts/**" -x "node_modules/**" -x ".DS_Store"
 
 # Install required dependencies for Python script.
 pip3 install boto3
