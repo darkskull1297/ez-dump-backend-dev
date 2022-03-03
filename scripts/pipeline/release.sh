e33796baa73b93615dd8c0fd52f8afee74d1d2c8
darkskull1297 <77977994+darkskull1297@users.noreply.github.com>
2022-03-03 23:10:29 +0100

    Sync

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -8,7 +8,7 @@ local_path=$5
 rm -rf $local_path
 
 # Create a zip of the current directory.
-zip -r $local_path . -x "*.git/" -x ".git/**" -x "*.github/" -x ".github/**" -x "*src/" -x "src/**" -x "*.idea/" -x ".idea/**" -x "*.vscode/" -x ".vscode/**" -x "*@types/" -x "@types/**" -x "*test/" -x "test/**" -x "*.eslintrc.js" -x ".eslintrc.js" -x "*node_modules/" -x "node_modules/**" -x "*.prettierrc" -x ".prettierrc" -x "*nest-cli.json" -x "nest-cli.json" -x "*README.md" -x "README.md" -x "*tsconfig.build.json" -x "tsconfig.build.json" -x "*tsconfig.json" -x "tsconfig.json" -x "*yarn.lock" -x "yarn.lock" -x "*.gitignore" -x ".gitignore" -x "*scripts/" -x "scripts/**"
+zip -r $local_path . -x "*.git/" -x ".git/**" -x "*.github/" -x ".github/**" -x "*src/" -x "src/**" -x "*.idea/" -x ".idea/**" -x "*.vscode/" -x ".vscode/**" -x "*types/" -x "types/**" -x "*test/" -x "test/**" -x "*.eslintrc.js" -x ".eslintrc.js" -x "*node_modules/" -x "node_modules/**" -x "*.prettierrc" -x ".prettierrc" -x "*nest-cli.json" -x "nest-cli.json" -x "*README.md" -x "README.md" -x "*tsconfig.build.json" -x "tsconfig.build.json" -x "*tsconfig.json" -x "tsconfig.json" -x "*yarn.lock" -x "yarn.lock" -x "*.gitignore" -x ".gitignore" -x "*scripts/" -x "scripts/**"
 
 # Install required dependencies for Python script.
 pip3 install boto3
